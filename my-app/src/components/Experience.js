55b30e0822f94f4cbc9a61c6f6c3eaaefb4c5424
latka-krystof <74324358+latka-krystof@users.noreply.github.com>
2022-12-24 09:06:07 +0100

    improve leadership and experience pages

--- my-app/src/components/Experience.js
+++ my-app/src/components/Experience.js
@@ -1,17 +1,39 @@
 import React from 'react';
 
+import Button from '@mui/material/Button';
+import DownloadIcon from '@mui/icons-material/Download';
+import LinkIcon from '@mui/icons-material/Link';
+
+import Presentation from '../assets/Presentation.pdf';
+import LinkedIn from '../assets/LinkedIn.png';
+import Twitter from '../assets/Twitter.png';
+
 function Experience() {
     return (
         <div>
           <div className="masthead">
 						<h1>Experience</h1>
 					</div>
-          <h3>Open Mechanics Group, Czech Technical University</h3>
-					<p>
-						<a href="https://doi.org/10.14311/APP.2022.34.0032">Published paper</a><br />
-						<a href="https://gitlab.com/MartinDoskar/ai-based-reconstruction">Source code on GitLab</a>
-					</p>
-					<h3>Computational Robotics Laboratory, Artificial Intelligence Center, Czech Technical University</h3>
+          <h2>Open Mechanics Group, Czech Technical University</h2>
+					<hr style={{color: 'black'}}></hr>
+					<p>I conducted research on microstructure reconstruction via artificial neural networks in TensorFlow at the Department of Mechanics under the co-supervision of Professor Jan Zeman and Dr. Martin Doškář. I evaluated the impacts of different configurations of the neural network on the model’s predictive abilities and presented the results at the ”Nano and Micro Mechanics 2021” conference at Czech Technical University.</p>
+					<div style={{margin: '10px'}}>
+						<Button variant="outlined" href={Presentation} startIcon={<DownloadIcon />}>
+							Conference Presentation
+						</Button>
+					</div>
+					<div style={{margin: '10px'}}>
+						<Button variant="outlined" href="https://gitlab.com/MartinDoskar/ai-based-reconstruction" startIcon={<LinkIcon />}>
+							Source code on GitLab
+						</Button>
+					</div>
+					<h3>In media</h3>
+					<div className="flexRow" style={{justifyContent: 'space-evenly'}}>
+						<img src={LinkedIn} alt="" style={{width: '40%', height: 'auto'}} />
+						<img src={Twitter} alt="" style={{width: '40%', height: 'auto'}} />
+					</div>
+					<h2>Computational Robotics Laboratory, Artificial Intelligence Center, Czech Technical University</h2>
+					<hr style={{color: 'black'}}></hr>
         </div>
     )
 }
--- my-app/src/components/Leadership.js
+++ my-app/src/components/Leadership.js
@@ -1,18 +1,41 @@
 import React from 'react';
 
+import Button from '@mui/material/Button';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import GetAppIcon from '@mui/icons-material/GetApp';
+import AndroidIcon from '@mui/icons-material/Android';
+
 function Leadership() {
     return (
         <div>
 					<div className="masthead">
 						<h1>Leadership</h1>
 					</div>
-					<h3>Vervit</h3>
+					<h2>Vervit</h2>
+					<hr style={{color: 'black'}}></hr>
+					<h3>Chief App Developer</h3>
 					<p>
-						Vervit is a non-profit organization, focused on helping elderly citizens in Czech Republic in working with techonology. I lead a programming team which developed a mobile application, freely available on iOS and Android, that provided a user-friendly environment for senior citizens to learn new IT skills.<br />
-						<a href="https://www.youtube.com/channel/UC2wYlSqcqfmzFz_5J1s0jHA">YouTube Channel</a><br />
-						<a href="https://apps.apple.com/us/app/vervit/id1586873318">iOS App</a><br />
-						<a href="https://play.google.com/store/apps/details?id=com.vervit.vervit_app">Android App</a>
+						I held a key role in Vervit, a student non-profit organization providing technological support to senior citizens in the Czech Republic, primarily through YouTube videos. As the Chief App Developer, I primarily managed a team of six programmers and developed a mobile application, freely available on iOS and Android, which provided a user-friendly learning environment for senior citizens. I also contributed to creating about fifty tutorial videos by writing scripts and editing videos.
 					</p>
+					<div style={{margin: '10px'}}>
+						<Button variant="outlined" href="https://www.youtube.com/channel/UC2wYlSqcqfmzFz_5J1s0jHA" startIcon={<YouTubeIcon />}>
+							Visit our YouTube channel
+						</Button>
+					</div>
+					<div style={{margin: '10px'}}>
+						<Button variant="outlined" href="https://apps.apple.com/us/app/vervit/id1586873318" startIcon={<GetAppIcon />}>
+							Download our app on iOS
+						</Button>
+					</div>
+					<div style={{margin: '10px'}}>
+						<Button variant="outlined" href="https://play.google.com/store/apps/details?id=com.vervit.vervit_app" startIcon={<AndroidIcon />}>
+							Download our app on Android
+						</Button>
+					</div>
+					<h2>ECONET</h2>
+					<hr style={{color: 'black'}}></hr>
+					<h3>Vice-President and Co-Founder of Novy PORG Investment Society</h3>
+					<p>The Novy PORG Investment Society analyzes companies on the stock market. Apart from that, we also strived to raise financial awareness among other students in my high school. As a Vice President of the Society, I mentored a team of new recruits and held lectures on introductory topics in investing. In 2020, our team qualified for the regional finale of the Wharton Investment Competition.</p>
         </div>
     )
 }
